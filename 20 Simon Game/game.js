9b8e8ec70f53ddbfc60d2a8f5691a2686f7cb9cd
mzaferb <bildik@yandex.com>
2023-11-22 10:30:25 +0300

    Simon Game Playable

--- 20 Simon Game/game.js
+++ 20 Simon Game/game.js
@@ -1,20 +1,66 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
-
-var gamePattern = [];
-
-var userClickedPattern = [];
+var colors = ["green", "red", "yellow", "blue"];
+var gameList = [];
+var playerList = [];
+var startKey = true;
+var level = 0;
 
 $(".btn").click(function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-  });
+    var click = $(this).attr("id");
+    playerList.push(click);
+    sound(click);
+    animateClick(click);
+    control();
+})
 
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  var sound1 = new Audio("sounds/" + randomChosenColour + ".mp3");
-  sound1.play();
-};
+function control() {
+
+    var pLength = playerList[(playerList.length)-1] === gameList[(playerList.length)-1]
+
+    if (pLength && gameList.length == playerList.length) {
+        setTimeout(function() {newColor();}, 500);
+    }
+    else if (pLength && gameList.length != playerList.length) {
+        return;
+    }
+    else {
+        gameOver();
+    }
+}
+
+function newColor() {
+    var color = colors[Math.floor(Math.random()*4)];
+    gameList.push(color);
+    $("." + color).fadeOut(100).fadeIn(100);
+    sound(color);
+    level++;
+    $("#level-title").text("Level " + level);
+    playerList.length = 0;
+}
+
+function sound (name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animateClick(name) {
+    $("#" + name).addClass("pressed");
+    setTimeout(function() {
+        $("#" + name).removeClass("pressed")
+    }, 100);
+}
+
+$(document).keydown(function() {
+    if (startKey) {
+        $(".btn").removeClass("pressed");
+        newColor();
+        startKey = false;
+    };
+})
+
+function gameOver() {
+    $("#level-title").text("Game Over at Level " + level);
+    $(".btn").addClass("gameover");
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    setTimeout(function() {window.location.reload();}, 4000);
+}
